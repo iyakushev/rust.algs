7cea98bdb4463ca8c65e27e6929fb3d994e21322
iy <iamaero7@gmail.com>
2020-01-08 00:28:20 +0300

    Implemented quicksort

---
+++ src/sorting/quick.rs
@@ -0,0 +1,37 @@
+mod random;
+
+fn partition(v: &mut [f64]) -> usize {
+    let pivot = v.len() / 2;
+    let last_index = v.len() - 1;
+ 
+    v.swap(pivot, last_index);  // Move pivot to the end of the vec
+ 
+    let mut border = 0;
+    for i in 0..last_index {
+        if v[i] < v[last_index] {
+            v.swap(i, border);  // Move everything below pivot to the left
+            border += 1;       // Update the border value for the left part
+        }
+    }
+ 
+    v.swap(border, last_index); // Swap border value with pivot
+    border
+}
+
+
+fn quick_sort(v: &mut [f64]) {
+    if v.len() <= 1 {return}
+    
+    let pivot = partition(v);
+
+    quick_sort(&mut v[..pivot]);
+    quick_sort(&mut v[pivot..]);
+}
+
+
+fn main() {
+    let mut v = random::generate_vec(10, -10., 30.);
+    println!("BEFORE: {:?}", v);
+    quick_sort(v.as_mut_slice());
+    println!("AFTER: {:?}", v);
+}
